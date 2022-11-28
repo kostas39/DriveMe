88cd85941a137fecdcecfb963748a5197b4be412
Victor de Quercize <vdequercize@hotmail.com>
2022-11-28 16:52:05 +0100

    added the booking

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,42 @@
+class BookingsController < ApplicationController
+  def new
+    @car = Car.find(params[:id])
+    @booking = Booking.new
+  end
+
+  def create
+    @car = Car.find(params[:id])
+    @booking = Booking.new(booking_params)
+    @booking.car = @car
+    if @booking.save
+      redirect_to car_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @booking = Booking.Find(params[:id])
+  end
+
+  def update
+    @booking = Booking.Find(params[:id])
+    if @booking.update(booking_params)
+      redirect_to car_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @booking = Booking.Find(params[:id])
+    @booking.destroy
+    redirect_to car_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:car_id, :user_id, :start_date, :end_date)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
+##  get 'bookings/new'
+##  get 'bookings/create'
+##  get 'bookings/edit'
+##  get 'bookings/update'
+##  get 'bookings/destroy'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  
+
+  resourcers :car do
+    resources :booking, only: [:new, :create]
+  end
+
+  resources :booking, only: [:edit, :update, :destroy]
 end
