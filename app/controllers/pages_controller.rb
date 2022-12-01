0f326dc24a09069769aa9f755245abfe7f02cb3f
arnold cubici-jones <arnoldcubicijones@hotmail.co.uk>
2022-12-01 16:48:39 +0000

    Removes redundancies

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,20 +13,4 @@ class PagesController < ApplicationController
   def profile
     @user = current_user
   end
-
-  def update
-    @user.update(user_params)
-    redirect_to profile_path
-
-    # @car = Car.find(params[:id])
-    # authorize @car
-    # @car.update(car_params)
-    # redirect_to car_path(@car)
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :avatar)
-  end
 end
